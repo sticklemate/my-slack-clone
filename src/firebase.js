7cac21d34b5eec408a83542f605558a860eaf9a6
sticklemate <sticklemate@gmail.com>
2021-03-14 11:19:13 +1100

    initial commit react boilerplate

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjiQmXVf9_m9PA6jO2gE6H0rLvGBpMAbI",
+    authDomain: "slack-clone-sticklemate.firebaseapp.com",
+    projectId: "slack-clone-sticklemate",
+    storageBucket: "slack-clone-sticklemate.appspot.com",
+    messagingSenderId: "793467309183",
+    appId: "1:793467309183:web:c30145792798acb748434a"
+  };
